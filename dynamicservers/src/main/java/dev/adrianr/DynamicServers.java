f7bf05518b36550f8ddec8f7ca4cc6c06305bbca
AdrianR25 <35237320+AdrianR25@users.noreply.github.com>
2022-01-20 15:55:07 +0100

    Added test command & renamed bungee.yml

--- dynamicservers/src/main/java/dev/adrianr/DynamicServers.java
+++ dynamicservers/src/main/java/dev/adrianr/DynamicServers.java
@@ -1,10 +1,12 @@
 package dev.adrianr;
 
+import dev.adrianr.commands.TestCommand;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class DynamicServers extends Plugin {
     @Override
     public void onEnable() {
-        
+        ProxyServer.getInstance().getPluginManager().registerCommand(this, new TestCommand());
     }
 }
---
+++ dynamicservers/src/main/java/dev/adrianr/commands/TestCommand.java
@@ -0,0 +1,21 @@
+package dev.adrianr.commands;
+
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+public class TestCommand extends Command {
+
+    public TestCommand() {
+        super("dstest");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        sender.sendMessage(new ComponentBuilder("Executing test command").create());
+        
+    }
+
+}
